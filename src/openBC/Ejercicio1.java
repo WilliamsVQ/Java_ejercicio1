6fd3a5ca2dd85b778794e1a28d0d75030182189f
corvuspdg <williamsvq254@gmail.com>
2022-09-30 21:22:03 -0400

    some init msg

---
+++ src/openBC/Ejercicio1.java
@@ -0,0 +1,12 @@
+package openBC;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int a = 9; long b = 2147483647; double c = 34.54; boolean d = true; String e = "End";
+        System.out.println("Entero = "+a);
+        System.out.println("Long =" +b);
+        System.out.println("Double = "+c);
+        System.out.println("Boolean = "+d);
+        System.out.println("String = "+e);
+    }
+}
